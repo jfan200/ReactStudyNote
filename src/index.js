e72f534cbca2e75ab8b31a70265289ba8027bcee
Jinhua Fan <76262267+jfan200@users.noreply.github.com>
2024-07-22 02:41:34 +1000

    updated App.js page

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ import Hook from "./Hook";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
-    <Comments/>
+    <App/>
   //</React.StrictMode>
 );

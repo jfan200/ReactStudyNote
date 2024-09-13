a37ee18b808e8449536aacba7b7ad76313b48b3b
Jinhua <228871192@qq.com>
2024-09-14 03:12:57 +1000

    innit commit
    Co-Authored-By: Jinhua Fan <76262267+jfan200@users.noreply.github.com>

--- react-cp/src/router/index.js
+++ react-cp/src/router/index.js
@@ -1,59 +1,19 @@
 // 路由配置
+import Layout from "@/pages/Layout";
+import Login from "@/pages/Login";
 import {createBrowserRouter} from "react-router-dom";
 import { AuthRoute } from "@/component/AuthRoute";
-import Dashboard from "@/pages/Dashboard";
-import Building from "src/pages/Building";
-import CPLayout from "src/pages/Layout";
-import Staff from "@/pages/Staff";
-import Login from "@/pages/Login";
-import NoFound from "antd/es/result/noFound";
-import index from "@/pages/Building/BuildingTask";
-import BuildingTask from "@/pages/Building/BuildingTask";
-import StaffInfo from "@/pages/Staff/StaffInfo";
-import StaffNew from "@/pages/Staff/StaffNew";
-
-
 
 // 配置路由实例
 const router = createBrowserRouter(
     [
         {
             path: "/",
-            element:<AuthRoute> <CPLayout /> </AuthRoute>,
-            children: [
-                {
-                    index: true,
-                    element: <Dashboard />
-                },
-                {
-                    path: "staff",
-                    element: <Staff />
-                },
-                {
-                    path: "staff/StaffInfo",
-                    element: <StaffInfo />
-                },
-                {
-                    path: "staff/new",
-                    element: <StaffNew />
-                },
-                {
-                    path: "building",
-                    element: <Building />,
-                },
-                {
-                    path: "building/BuildingTask",
-                    element: <BuildingTask />
-                }
-            ]
+            element:<AuthRoute> <Layout/> </AuthRoute>
         },
         {
             path: "/login",
             element: <Login />
-        },
-        {
-            path: '*',
-            element: <NoFound />
         }
     ]
 )
--- react-cp/src/utils/index.js
+++ react-cp/src/utils/index.js
@@ -1,16 +1,12 @@
 // 统一中转工具模块函数
+// import { request } form '@/utils'
 
 import { request } from './request'
 import {getBasicAuth, setBasicAuth, removeBasicAuth} from "@/utils/basicAuth";
-import {setInfo, getUserName, getUserID, removeUserInfo} from "@/utils/userInfo";
 
 export {
     request,
     getBasicAuth,
     setBasicAuth,
-    removeBasicAuth,
-    setInfo,
-    getUserName,
-    getUserID,
-    removeUserInfo
+    removeBasicAuth
 }

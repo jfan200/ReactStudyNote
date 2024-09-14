c403a82f9e081163b11e185ded44c7453b747606
Jinhua <228871192@qq.com>
2024-09-14 15:14:14 +1000

    Init commit

---
+++ react-cp/src/router/index.js
@@ -0,0 +1,21 @@
+// 路由配置
+import Layout from "@/pages/Layout";
+import Login from "@/pages/Login";
+import {createBrowserRouter} from "react-router-dom";
+import { AuthRoute } from "@/component/AuthRoute";
+
+// 配置路由实例
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element:<AuthRoute> <Layout/> </AuthRoute>
+        },
+        {
+            path: "/login",
+            element: <Login />
+        }
+    ]
+)
+
+export default router;

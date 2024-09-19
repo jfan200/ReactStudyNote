0d00fb2541150885eeb2c928d46951c2561074dd
Jinhua <228871192@qq.com>
2024-09-19 13:27:20 +1000

    feat: Added models
    - Added models for User, Building, Task, Job, Feedback, and Image.

---
+++ cp_backend/Models/Building.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace cp_backend.Models;
+
+
+public class Building
+{
+    [BsonId]
+    public ObjectId _id { get; set; }
+    public long building_id { get; set; }
+    public IEnumerable<long> internal_ids { get; set; }
+    public IEnumerable<long> subcontractor_ids { get; set; }
+    public IEnumerable<long> cleaner_ids { get; set; }
+    public IEnumerable<long> task_ids { get; set; }
+    public Building_info building_info { get; set; }
+}
+
+
+public class Building_info
+{
+    public string building_name { get; set; }
+    public string company_name { get; set; }
+    public string owner_corporation { get; set; }
+    public string address { get; set; }
+    public string suburb { get; set; }
+    public string state { get; set; }
+    public string postal_code { get; set; }
+    public string building_level { get; set; }
+    public string contract_start_date {get; set;}
+    public string contract_end_date {get; set;}
+    public Contact_info contact_info {get; set;}
+}
+
+
+public class Contact_info
+{
+    public string first_name { get; set; }
+    public string last_name { get; set; }
+    public string phone { get; set; }
+    public string email { get; set; }
+}
---
+++ cp_backend/Models/BuildingTask.cs
@@ -0,0 +1,21 @@
+﻿namespace cp_backend.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+public class BuildingTask
+{
+    [BsonId]
+    public ObjectId _id { get; set; }
+    public long task_id { get; set; }
+    public long building_id { get; set; }
+    public long internal_id { get; set; }
+    public long subcontractor_id { get; set; }
+    public long cleaner_id { get; set; }
+    public string date { get; set; }
+    public IEnumerable<long> job_ids { get; set; }
+    
+    public string comment_id { get; set; }
+    public string internal_feedback_id { get; set; }
+    public string subcontractor_feedback_id { get; set; }
+}
